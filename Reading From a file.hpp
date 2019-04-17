92eeaa0071a49c2b8b196c71b35ce65a033b1298
Yeltzinc <35970214+Yeltzinc@users.noreply.github.com>
2019-04-16 23:37:59 -0700

    Add files via upload

---
+++ Reading From a file.hpp
@@ -0,0 +1,13 @@
+//
+//  Reading From a file.hpp
+//  
+//
+//  Created by Yeltz on 2/21/19.
+//
+
+#ifndef Reading_From_a_file_hpp
+#define Reading_From_a_file_hpp
+
+#include <stdio.h>
+
+#endif /* Reading_From_a_file_hpp */
